12176a6e13222e2439e3d93e24b1c516eceea069
Pierre Rossato <pierre.rossato@soprasteria.com>
2020-12-07 14:01:55 +0100

    ✨🎨 Day3-P2

--- src/test/kotlin/org/github/pirocraft/adventofcode/Day3TrajectoryShould.kt
+++ src/test/kotlin/org/github/pirocraft/adventofcode/Day3TrajectoryShould.kt
@@ -6,10 +6,37 @@ import org.junit.jupiter.api.Test
 const val TREE = '#'
 
 class Day3TrajectoryShould {
+    private val tobogganOnExample = Toboggan(mapExample.split("\n"))
+
     @Test
     internal fun `count trees met during descent`() {
-        assertEquals(7, Toboggan(mapExample.split("\n")).howManyTreesMet(Point(3, 1)))
+        assertEquals(7, tobogganOnExample.howManyTreesMet(Point(3, 1)))
         assertEquals(169, Toboggan(InputLoader.loadInput("slopeInput")).howManyTreesMet(Point(3, 1)))
+    }
+
+    @Test
+    internal fun `count cumulative trees met during several descent`() {
+        val slope1 = Point(1, 1)
+        val slope2 = Point(3, 1)
+        val slope3 = Point(5, 1)
+        val slope4 = Point(7, 1)
+        val slope5 = Point(1, 2)
+
+        assertEquals(2, tobogganOnExample.howManyTreesMet(slope1))
+        assertEquals(7, tobogganOnExample.howManyTreesMet(slope2))
+        assertEquals(3, tobogganOnExample.howManyTreesMet(slope3))
+        assertEquals(4, tobogganOnExample.howManyTreesMet(slope4))
+        assertEquals(2, tobogganOnExample.howManyTreesMet(slope5))
+        assertEquals(
+            336,
+            tobogganOnExample.howManyTreesMetForManySlopes(listOf(slope1, slope2, slope3, slope4, slope5))
+        )
+        assertEquals(
+            7560370818,
+            Toboggan(InputLoader.loadInput("slopeInput")).howManyTreesMetForManySlopes(
+                listOf(slope1, slope2, slope3, slope4, slope5)
+            )
+        )
     }
 
     data class Point(val x: Int, val y: Int) {
@@ -19,6 +46,9 @@ class Day3TrajectoryShould {
     class Toboggan(private val map: List<String>) {
         private val mapWidth = map[0].length
         private val mapHeight = map.size
+
+        fun howManyTreesMetForManySlopes(slopes: List<Point>): Long =
+            slopes.fold(1.toLong()) { acc, point -> acc * howManyTreesMet(point) }
 
         fun howManyTreesMet(move: Point): Int =
             howManyTreesMet(Point(0, 0) + move, move)
@@ -37,15 +67,16 @@ class Day3TrajectoryShould {
 
     }
 
-    private val mapExample = "..##.......\n" +
-            "#...#...#..\n" +
-            ".#....#..#.\n" +
-            "..#.#...#.#\n" +
-            ".#...##..#.\n" +
-            "..#.##.....\n" +
-            ".#.#.#....#\n" +
-            ".#........#\n" +
-            "#.##...#...\n" +
-            "#...##....#\n" +
-            ".#..#...#.#"
 }
+
+const val mapExample = "..##.......\n" +
+        "#...#...#..\n" +
+        ".#....#..#.\n" +
+        "..#.#...#.#\n" +
+        ".#...##..#.\n" +
+        "..#.##.....\n" +
+        ".#.#.#....#\n" +
+        ".#........#\n" +
+        "#.##...#...\n" +
+        "#...##....#\n" +
+        ".#..#...#.#"
